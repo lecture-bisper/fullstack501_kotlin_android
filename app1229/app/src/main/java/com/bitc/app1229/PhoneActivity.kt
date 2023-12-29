8badebe7904d0fefd0b186489a08e3969b4f386f
lecture-bisper <bisper@outlook.kr>
2023-12-29 16:47:31 +0900

    12/29 layout 사용하기, 여러가지 Layout 으로 전화 키패드 구현하기.

---
+++ app1229/app/src/main/java/com/bitc/app1229/PhoneActivity.kt
@@ -0,0 +1,93 @@
+package com.bitc.app1229
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bitc.app1229.databinding.ActivityPhoneBinding
+
+class PhoneActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    val binding = ActivityPhoneBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    var phoneNumber: String = ""
+
+    binding.tv0.setOnClickListener {
+      phoneNumber = binding.tvPhoneNumber.text as String
+      phoneNumber += binding.tv0.text
+      binding.tvPhoneNumber.text = phoneNumber
+    }
+
+    binding.tv1.setOnClickListener {
+      phoneNumber = binding.tvPhoneNumber.text as String
+      phoneNumber += binding.tv1.text
+      binding.tvPhoneNumber.text = phoneNumber
+    }
+
+    binding.tv2.setOnClickListener {
+      phoneNumber = binding.tvPhoneNumber.text as String
+      phoneNumber += binding.tv2.text
+      binding.tvPhoneNumber.text = phoneNumber
+    }
+
+    binding.tv3.setOnClickListener {
+      phoneNumber = binding.tvPhoneNumber.text as String
+      phoneNumber += binding.tv3.text
+      binding.tvPhoneNumber.text = phoneNumber
+    }
+
+    binding.tv4.setOnClickListener {
+      phoneNumber = binding.tvPhoneNumber.text as String
+      phoneNumber += binding.tv4.text
+      binding.tvPhoneNumber.text = phoneNumber
+    }
+
+    binding.tv5.setOnClickListener {
+      phoneNumber = binding.tvPhoneNumber.text as String
+      phoneNumber += binding.tv5.text
+      binding.tvPhoneNumber.text = phoneNumber
+    }
+
+    binding.tv6.setOnClickListener {
+      phoneNumber = binding.tvPhoneNumber.text as String
+      phoneNumber += binding.tv6.text
+      binding.tvPhoneNumber.text = phoneNumber
+    }
+
+    binding.tv7.setOnClickListener {
+      phoneNumber = binding.tvPhoneNumber.text as String
+      phoneNumber += binding.tv7.text
+      binding.tvPhoneNumber.text = phoneNumber
+    }
+
+    binding.tv8.setOnClickListener {
+      phoneNumber = binding.tvPhoneNumber.text as String
+      phoneNumber += binding.tv8.text
+      binding.tvPhoneNumber.text = phoneNumber
+    }
+
+    binding.tv9.setOnClickListener {
+      phoneNumber = binding.tvPhoneNumber.text as String
+      phoneNumber += binding.tv9.text
+      binding.tvPhoneNumber.text = phoneNumber
+    }
+
+    binding.tvAsterisk.setOnClickListener {
+      phoneNumber = binding.tvPhoneNumber.text as String
+      phoneNumber += binding.tvAsterisk.text
+      binding.tvPhoneNumber.text = phoneNumber
+    }
+
+    binding.tvSharp.setOnClickListener {
+      phoneNumber = binding.tvPhoneNumber.text as String
+      phoneNumber += binding.tvSharp.text
+      binding.tvPhoneNumber.text = phoneNumber
+    }
+
+    binding.imgBack.setOnClickListener {
+      phoneNumber = binding.tvPhoneNumber.text as String
+      phoneNumber = phoneNumber.substring(0, phoneNumber.length - 1)
+      binding.tvPhoneNumber.text = phoneNumber
+    }
+  }
+}
