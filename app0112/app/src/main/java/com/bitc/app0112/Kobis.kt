a76badc5c9e210e6d58ce09ceab3a5fa78170d97
lecture-bisper <bisper@outlook.kr>
2024-01-12 14:44:38 +0900

    01/12 폰 정보 가져오기, 네트워크 상태 확인, volley로 서버와 통신하기

---
+++ app0112/app/src/main/java/com/bitc/app0112/Kobis.kt
@@ -0,0 +1,18 @@
+package com.bitc.app0112
+
+import com.google.gson.annotations.SerializedName
+
+data class Kobis (
+  @SerializedName("boxOfficeResult" )
+  var boxOfficeResult : BoxOfficeResult?
+)
+
+
+
+
+
+
+
+
+
+
